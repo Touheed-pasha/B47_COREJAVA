e5a422d23ca9f8b15af9f5117a0803d370a4077a
LENOVO <LENOVO@DESKTOP-09IMB9F>
2023-03-15 16:45:04 +0530

    first commit

---
+++ touheedapplication/src/com/tnsif/vvit/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.tnsif.vvit;
+/*
+ * program to demonstrate hello world
+ */
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub // Single Line Comment
+        System.out.println("Hello world");
+	}
+
+}
