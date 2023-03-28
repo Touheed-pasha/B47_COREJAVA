2acfb44dc549e952b9784fd594ed010feee1c0ee
LENOVO <LENOVO@DESKTOP-09IMB9F>
2023-03-28 13:09:43 +0530

    Final keyword

---
+++ touheedapplication/src/com/tns/finalkeyword/Abc.java
@@ -0,0 +1,18 @@
+package com.tns.finalkeyword;
+//class as final
+final class Xyz{
+	final void display() {
+		
+	}
+}
+
+public class Abc extends Xyz {
+	final int a=10;
+	Abc(){
+		//a++;
+	}
+	//method as final
+	final void display(){
+		
+	}
+}
